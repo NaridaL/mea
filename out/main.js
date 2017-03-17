421685b8aa924c89e4448d25a1643ca3f92f519e
Adrian Leonhard <adrianleonhard@gmail.com>
2017-03-17 05:01:55 +0100

    init

---
+++ out/main.js
@@ -0,0 +1,172 @@
+const MooEl = Element;
+function valuesFromHash(hash) {
+    const values = {};
+    getAllWeapons().forEach(w => { assert(!values.hasOwnProperty(w.sn), w.sn), values[w.sn] = 0; });
+    mpchars.forEach(w => { assert(!values.hasOwnProperty(w.sn), w.sn), values[w.sn] = w.startlvl || 0; });
+    const regex = /([A-Za-z]+)([0-9]{1,2})/g;
+    let match;
+    while (match = regex.exec(hash)) {
+        const value = parseInt(match[2]);
+        const sn = match[1];
+        if (1 <= value && value <= 10 && values.hasOwnProperty(sn)) {
+            values[sn] = value;
+        }
+    }
+    return values;
+}
+const roman = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+function hashFromValues(values) {
+    let c;
+    return Object.getOwnPropertyNames(values)
+        .filter(sn => values[sn] !== ((c = mpchars.find(e => e.sn == sn)) && c.startlvl || 0))
+        .map(sn => sn + values[sn])
+        .join('');
+}
+function getAllWeapons() {
+    return weaponTypes.map(cat => cat.ws).concatenated();
+}
+function createWeaponDisplay() {
+    const weaponsDiv = $('weapons');
+    weaponTypes.forEach(cat => {
+        const wcatDiv = template('templateWCat', { wcat: cat.type }).inject(weaponsDiv);
+        cat.ws.forEach(w => {
+            template('templateWeapon', w)
+                .addEvent('click', incWeapon)
+                .addEvent('mousedown', e => e.preventDefault()) // stop highlighting
+                .inject(wcatDiv);
+        });
+    });
+}
+function createCharDisplay() {
+    const weaponsDiv = $('chars');
+    mpchars.forEach(w => {
+        template('templateChar', w)
+            .addEvent('click', incChar)
+            .addEvent('mousedown', e => e.preventDefault()) // stop highlighting
+            .inject(weaponsDiv);
+    });
+}
+function incWeapon(e) {
+    const sn = e.event.currentTarget.id.match(/sn-(.*)/)[1];
+    assert(sn);
+    const newValue = (values[sn] + 1) % 11;
+    values[sn] = newValue;
+    save();
+    updateWeapon(sn, newValue);
+}
+function incChar(e) {
+    const sn = e.event.currentTarget.id.match(/sn-(.*)/)[1];
+    assert(sn);
+    const newValue = Math.max((values[sn] + 1) % 6, mpchars.find(c => c.sn == sn).startlvl || 0);
+    values[sn] = newValue;
+    save();
+    updateChar(sn, newValue);
+}
+function template(templateName, map) {
+    let html = $(templateName).text;
+    for (const key in map) {
+        html = html.replace(new RegExp('\\$' + key, 'g'), map[key]);
+    }
+    return $(new MooEl('div', { html: html.trim() }).firstChild);
+}
+function save() {
+    document.title = 'MEA MP ' + prog(totalUnlocks(values), maxUnlocks());
+    let hash = hashFromValues(values);
+    if (modeHash) {
+        window.location.hash = '#' + hash;
+    }
+    else {
+        document.cookie = 'unlocks=' + hash + ';expires=' + new Date(2185, 10, 7).toUTCString();
+        console.log(hash);
+    }
+    updateSaveMode();
+}
+function totalUnlocks(values) {
+    return (getAllWeapons().map(({ sn }) => values[sn]).sum()
+        + mpchars.map(c => values[c.sn] - (c.startlvl || 0)).sum());
+}
+function maxUnlocks() {
+    return (getAllWeapons().length * 10
+        + mpchars.map(c => 5 - (c.startlvl || 0)).sum());
+}
+function updateWeapon(sn, value) {
+    let wdiv = $('sn-' + sn);
+    wdiv.getElements('.bullet').forEach((el, i) => {
+        el.toggleClass('on', i < value);
+    });
+    wdiv.getElement('.wlvl').set('text', value ? roman[value - 1] : '');
+    const wcat = wdiv.getParent().id.match(/wcat-(.*)/)[1];
+    const ww = weaponTypes.find(wt => wt.type == wcat).ws;
+    wdiv.getParent().getElement('.prog').set('text', prog(ww.map(w => values[w.sn]).sum(), ww.length * 10));
+}
+function prog(prog, total) {
+    return prog + '/' + total + ' ' + Math.round(prog / total * 100) + '%';
+}
+function updateChar(sn, value) {
+    let wdiv = $('sn-' + sn);
+    wdiv.getElements('.bullet').forEach((el, i) => {
+        el.toggleClass('on', i < value);
+    });
+    const maxUnlocks = mpchars.map(c => 5 - (c.startlvl || 0)).sum();
+    const unlocks = mpchars.map(c => values[c.sn] - (c.startlvl || 0)).sum();
+    $('charTotal').set('text', prog(unlocks, maxUnlocks));
+}
+function updateSaveMode() {
+    let hash = hashFromValues(values);
+    if (hash) {
+        if (modeHash) {
+            $('saveinfo').set('html', 'Saving to hash. <a href="javascript:saveToCookie()">save to cookie instead</a>');
+        }
+        else {
+            $('saveinfo').set('html', 'Saving to cookie. <a href="#' + hash + '">share-/bookmarkable version</a>');
+        }
+    }
+    else {
+        $('saveinfo').set('html', '');
+    }
+}
+let values = {};
+let modeHash = !!window.location.hash;
+window.onload = function () {
+    createWeaponDisplay();
+    createCharDisplay();
+    load();
+};
+function saveToCookie() {
+    const match = document.cookie && document.cookie.match(/unlocks=(.*)/), cookieHash = match && match[1];
+    let cookieValues = valuesFromHash(cookieHash || '');
+    const cookiePercent = Math.round(totalUnlocks(cookieValues) / maxUnlocks() * 100);
+    const hashPercent = Math.round(totalUnlocks(valuesFromHash(window.location.hash.substr(1))) / maxUnlocks() * 100);
+    if (!cookiePercent || confirm(`Overwrite current cookie (${cookiePercent}% unlocked) with the hash (${hashPercent}% unlocked)?`)) {
+        window.location.hash = '';
+        values = cookieValues;
+        getAllWeapons().forEach(w => {
+            updateWeapon(w.sn, values[w.sn]);
+        });
+        mpchars.forEach(w => {
+            updateChar(w.sn, values[w.sn]);
+        });
+        modeHash = false;
+        save();
+    }
+}
+function load() {
+    if (window.location.hash && window.location.hash.substr(1)) {
+        values = valuesFromHash(window.location.hash.substr(1));
+        modeHash = true;
+    }
+    else {
+        const match = document.cookie && document.cookie.match(/unlocks=(.*)/), cookieHash = match && match[1];
+        values = valuesFromHash(cookieHash || '');
+        modeHash = false;
+    }
+    save();
+    getAllWeapons().forEach(w => {
+        updateWeapon(w.sn, values[w.sn]);
+    });
+    mpchars.forEach(w => {
+        updateChar(w.sn, values[w.sn]);
+    });
+}
+window.addEventListener("hashchange", load, false);
+//# sourceMappingURL=main.js.map
