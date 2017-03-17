b4132197174bf33d0c0434f48706fbdc20740294
Adrian Leonhard <adrianleonhard@gmail.com>
2017-03-17 05:01:55 +0100

    init

---
+++ weapons.js
@@ -0,0 +1,27 @@
+var weaponTypes = [
+  {
+    'type': 'Sniper Rifles', 'ws': [
+    {'name': 'Widow', 'sn': 'Wid'},
+    {'name': 'Black Widow', 'sn': 'BWd'},
+    {'name': 'Viper', 'sn': 'Vpr', 'startlvl': 1},
+  ]
+  },
+  {
+    'type': 'Pistols', 'ws': [
+    {'name': 'Scorpion', 'sn': 'Scp'},
+    {'name': 'Carnifex', 'sn': 'Cfx'},
+    {'name': 'M-3 Predator', 'sn': 'Prd', 'startlvl': 1},
+  ]
+  },
+  {
+    'type': 'Shotguns', 'ws': [
+    {'name': 'M-23 Katana', 'sn': 'Kat', 'startlvl': 1},
+  ]
+  },
+  {
+    'type': 'Assault Rifles', 'ws': [
+    {'name': 'M-8 Avenger', 'sn': 'Avg', 'startlvl': 1},
+    {'name': 'Sabre', 'sn': 'Sbr'}
+  ]
+  }
+]
